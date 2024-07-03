c30ab0f3f40047af132374747ed2f41ad67a6023
rwils83 <rwils83@gmail.com>
2024-07-03 05:44:37 -0400

    completed module3

---
+++ module_3/flashcards.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from model import db
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    """
+    Route for the welcome page.
+
+    Returns:
+        str: A welcome message.
+    """
+    return render_template("welcome.html", message="This is a message from the view")
+
+@app.route("/card")
+def card_view():
+    card = db[0]
+    return render_template("card.html", card=card)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5001)
+
